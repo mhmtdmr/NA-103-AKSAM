35877605b5e57eed7b6da5442114ac71df27c773
Mehmet DEMİR <mail.mehmetdemir@gmail.com>
2022-01-13 19:22:42 +0300

    Attribute okuma eklendi. Ornek2 eklendi.

--- js3.js
+++ js3.js
@@ -22,6 +22,8 @@ let kutu3Elementi = document.getElementById("kutu3");
 kutu3Elementi.classList.remove("kutu");
 kutu3Elementi.innerHTML = "KUTU<br/>DEĞİLİM<br/>BEN!!!";
 
+
+// onclick metoduna atanacak fonksiyonu tanımladık.
 let sayac = 0;
 let sayacGoster = document.querySelector("section#kutu2 h3");
 sayacGoster.innerHTML=sayac;
@@ -31,9 +33,46 @@ let sayacArtir = function(artis=1)
     // alert("ÇALIŞTIM");
     sayac = sayac+artis;
     sayacGoster.innerHTML = sayac;
-}   
-// Bir section içerisinde 20 tane buton tanımlayınız.
-// her buton
+}
 
-"<button onclick='sayiGoster("+i+")') >"+i+"</button>"
+// // class'ı kutu olan elementleri getir.
+// let kutular = document.getElementsByClassName("kutu");
+// console.log(kutular);
+// // her bir kutu için döngü.
+// for (let kutu of kutular)
+// {
+//     kutu.innerHTML += "<p>Bu bir kutudur.</p>";
+// }
+
+
+
+// // Yukarıdaki seçimi querySelectorAll ile de yapabilirdik.
+// let kutuListesi = document.querySelectorAll(".kutu");
+// // her bir kutu için döngü.
+// for (let kutu of kutuListesi)
+// {
+//     kutu.innerHTML += "<p>Bu bir kutudur.</p>";
+// }
+
+// attribute erişimleri.
+let kutu0 = document.getElementById("kutu0");
+kutu0.setAttribute("data-kutuid","99"); // set
+// let kutu0id = kutu0.getAttribute("data-kutuid"); //get
+kutu0.dataset.kutuid = "1243";
+let kutu0id = kutu0.dataset.kutuid;
+// let kutu0onemsirasi = kutu0.getAttribute("data-onemsirasi");
+let kutu0onemsirasi = kutu0.dataset.onemsirasi;
+console.log(kutu0id);
+console.log(kutu0onemsirasi);
+
+
+
+
+
+
+
+
+
+
+
 
