9ea938961cefb9d6be0bf019e9c3cd780b273341
Mehmet DEMİR <mail.mehmetdemir@gmail.com>
2022-01-17 16:40:15 +0300

    OOP başladı.

---
+++ OOP1/OOP1/Araba.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class Araba
+    {
+        private static int ID_Counter = 0;
+        private int _ID { get; set; }
+
+        public int ID
+        {
+            get { return this._ID; } // readonly.
+        }
+        public string Marka { get; set; }
+        public string Model { get; set; }
+
+        private int _ModelYili { get; set; }
+        public int ModelYili {
+            get
+            {
+                return this._ModelYili;
+            }
+            set
+            {
+                this._ModelYili = value;
+                this.Yas = YasHesapla(value);
+            } 
+        }
+
+        public int Yas { get; set; }
+
+        private int YasHesapla(int modelYili)
+        {
+            int anlikYil = DateTime.Now.Year;
+            return anlikYil - modelYili;
+        }
+        public Araba()
+        {
+            ID_Counter++;
+            this._ID = ID_Counter;
+            Console.WriteLine($"Yapıcı metot çalıştı. ID:{this._ID}");
+        }
+        public Araba(string marka, string model, int modelYili)
+        {
+            ID_Counter++;
+            this._ID = ID_Counter;
+            this.Marka = marka;
+            this.Model = model;
+            this.ModelYili = modelYili;
+            Console.WriteLine($"Parametreli yapıcı metot çalıştı. ID:{this._ID}");
+        }
+    }
+}
---
+++ OOP1/OOP1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sayi = 0; // set
+            //Console.WriteLine(sayi); // get
+
+            Araba oto1 = new Araba();
+            oto1.Marka = "Mazda";
+            oto1.Model = "626";
+            oto1.ModelYili = 1997;
+            Console.WriteLine("oto1 yaş:"+oto1.Yas);
+
+            //Console.WriteLine(oto1.ID);
+
+            //Araba oto2 = new Araba("Mercedes","CLS500",2010);
+
+        }
+    }
+}
