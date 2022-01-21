e7ff466febb20137d9b13f7851114b99f1e77f27
Mehmet DEMİR <mail.mehmetdemir@gmail.com>
2022-01-21 18:28:52 +0300

    Controler'dan View'e veri taşıma anlatılıyor.

--- MVC/MVC_001/MVC_001/Controllers/HomeController.cs
+++ MVC/MVC_001/MVC_001/Controllers/HomeController.cs
@@ -11,12 +11,36 @@ namespace MVC_001.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            // ViewData ile View'e veri taşıma
+            ViewData["studentID"] = 10;
+            ViewData["studentName"] = "Jane";
+            ViewData["studentSurname"] = "Doe";
+
+            List<int> sayilar = new List<int>() { 15, 25, 35, 67, 89 };
+            ViewData["numbers"] = sayilar;
+
+            // ViewBag ile View'e veri taşıma
+            ViewBag.teacherID = 1;
+            ViewBag.teacherName = "Serdar";
+
+            // TempData ile View'e ve Action'a veri taşıma!!!!
+            TempData["degisken"] = "Tempdatadan herkese selamlar.";
+
+            return View(); // /Views/Home/Index.cshtml'i çalıştır.
+        }
+
+        public ActionResult Logout()
+        {
+            ViewBag.exitvb = "ViewBag ile Action'a veri gitmeeeeez!!!";
+            ViewData["exitvd"] = "ViewBag ile Action'a veri gitmeeeeez!!!";
+
+            TempData["exit"] = "ÇIKIŞ YAPILDI!!!";
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
         {
-            return View();
+            return View(); // /Views/Home/About.cshtml'i çalıştır.
         }
     }
 }
--- MVC/MVC_001/MVC_001/Controllers/StudentController.cs
+++ MVC/MVC_001/MVC_001/Controllers/StudentController.cs
@@ -4,19 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using MVC_001.Models;   // Models altındaki sınıfları kullanacağız.
+
 namespace MVC_001.Controllers
 {
     public class StudentController : Controller
     {
-        // GET: Student
+        
         public ActionResult Index()
-        {
-            return View();
+        {   // model yolu ile View'e veri gönderme
+
+            Student stdnt = new Student()
+            {
+                ID = 99,
+                Name = "Burak",
+                Surname = "Yılmaz"
+            };
+
+            return View(stdnt);// /Views/Student/Index.cshtml'i çalıştır.
         }
 
         public ActionResult About()
         {
-            return RedirectToAction("Index");
+            return RedirectToAction("Index"); // Yukarıdaki Index controller'ına yönlendir.
         }
     }
 }
